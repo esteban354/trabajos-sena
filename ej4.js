8e3140397e4e360edd274506085e654971ea8aa3
esteban354 <sarriaobando@gmail.com>
2025-02-22 16:23:25 -1200

    Delete ej4.js

--- ej4.js
+++
@@ -1,8 +0,0 @@
-let numero = Number(prompt("Ingrese el numero"))
-let deter = numero % 2
-
-if (deter === 0){
-    alert("El numero es par")
-}else if (deter === 1 ){
-    alert("El numero es impar")
-}
