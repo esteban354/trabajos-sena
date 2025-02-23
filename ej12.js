287c98f43bf147dca59b80e2c91a129c62d73e4f
esteban354 <sarriaobando@gmail.com>
2025-02-22 16:19:17 -1200

    Delete ej12.js

--- ej12.js
+++
@@ -1,7 +0,0 @@
-let defecto = prompt("Ingrese el modelo de su automovil").trim();
-
-if (defecto === "179", defecto === "119", defecto === "189", defecto === "195", defecto === "221", defecto === "780") {
-    alert("Su modelo de automovil es defectuoso");
-} else {
-    alert("Su modelo de automovil está bien");
-}
